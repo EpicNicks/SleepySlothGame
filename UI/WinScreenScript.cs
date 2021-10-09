5d59856ccb7fb2b8f7f8b47315589cb4ee6ca9af
IanW000 <90727502+IanW000@users.noreply.github.com>
2021-10-08 21:23:59 -0400

    Updated Main Menu

---
+++ UI/WinScreenScript.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WinScreenScript : MonoBehaviour
+{
+    public void GoToNextLevel()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
+    }
+
+    public void LoadLevel(string level)
+    {
+        SceneManager.LoadScene(level);
+    }
+}
