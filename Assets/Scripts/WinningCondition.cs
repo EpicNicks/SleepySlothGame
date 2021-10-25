bee1381c20f32f3f265d7c7693eccd5693a9dd42
EyadT <eyadayman@gmail.com>
2021-10-24 23:33:55 -0400

    whatever

---
+++ Assets/Scripts/WinningCondition.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinningCondition : MonoBehaviour
+{
+    public GameObject winningMenuUI;
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("WON");
+            winningMenuUI.SetActive(true);
+            //Time.timeScale = 0f;
+        }
+    }
+}
